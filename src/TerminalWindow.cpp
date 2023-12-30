c6e3fc92dc2e098dc9c93cffa7adc475deb054a2
Camila <contactcamilasanchez@gmail.com>
2023-12-30 16:20:29 -0300

    Terminal windows comment

--- src/TerminalWindow.cpp
+++ src/TerminalWindow.cpp
@@ -3,6 +3,7 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 
+// terminal output colors and styles
 #define RESET   "\033[0m"
 #define RED     "\033[31m"
 #define BOLD    "\033[1m"
@@ -21,6 +22,7 @@ void TerminalWindow::message(std::string message)
     std::cout << BOLD << message << RESET << std::endl;
 }
 
+// used reference from: https://stackoverflow.com/questions/1022957/getting-terminal-width-in-c
 int TerminalWindow::getConsoleWidth() {
     struct winsize size;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
